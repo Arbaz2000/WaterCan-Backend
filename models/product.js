0d7308da137044021ddff6b5a62c5c47aef95b0c
kajal sharma <kajalsharma86900@gmail.com>
2024-10-22 11:51:41 +0530

    code added

---
+++ models/product.js
@@ -0,0 +1,29 @@
+// Import the mongoose library
+const mongoose = require('mongoose');
+
+// Define the product schema
+const productSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true
+  },
+  productDescription: {
+    type: String,
+    required: true
+  },
+  productPrice: {
+    type: Number,
+    required: true
+  },
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'User'
+// },
+  // Add more fields as needed
+});
+
+// Create a Product model based on the product schema
+const Product = mongoose.model('Product', productSchema);
+
+// Export the Product model
+module.exports = Product;
---
+++ routes/routeRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const {createRoute,getRouteById,updateRouteWithCustomers,getDriverAdmin,getRoutes,getRouteAndCustomerForDriver, updateRouteWithCustomersFromAdmin, routesGet, routesUpdate} = require('../controllers/routeController');
+
+// Create a new customer
+router.post('/route', createRoute);
+// get route for admin display
+router.post('/get-route',getRoutes);
+router.post('/routesUpdate/:id',routesUpdate);
+router.get('/getroute/:id',routesGet);
+router.put('/update-route/:routeId',updateRouteWithCustomersFromAdmin);
+// route.get('/route/:id',getRouteById);
+router.get('/get-all-admin-assigned/to/:driverId',getDriverAdmin)
+router.post('/add-customer/to/:adminId/:routeId',updateRouteWithCustomers)
+//get route for given driverId
+
+router.get('/route/:driverId',getRouteAndCustomerForDriver)
+
+
+module.exports = router;
