e188b530b626f4e7ca51fabc7525ec2c6b81ab5c
Prakash Gupta <prakashgupta272001@gmail.com>
2024-10-30 17:14:13 +0530

    test route api comments updated

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -6,18 +6,18 @@ const {createTransaction,transactionHistory,customerTransactions} = require('../
 
 // Create a new transcation
 router.post('/transaction/:userId', createTransaction);
+
+
 //Get All Transaction
+// !500 Internal Server Error The server has encountered a situation it does not know how to handle.
 router.get('/transaction-history/:userId',transactionHistory);
+
+// !500 Internal Server Error The server has encountered a situation it does not know how to handle.
 router.get('/transaction/:userId/:customerId',customerTransactions);
 
 // router.post('/transaction/send-email',generateEmailToCustomer);
 // Get a Transaction by ID
 // router.get('/transaction/:id', getTransactionById);
-
-
-
-
-
 
 
 module.exports=router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,32 +6,44 @@ const {createUser,getUserById,updateUser,deleteUser,verifyUserCredentials, addPr
 const { getAllProducts, addProduct } = require('../controllers/productController');
 
 // Create a new User
+// !404 Not Found Requested resource could not be found. 😐
 router.post('/users', createUser);
 
 // Get a User by ID
+// !500 Internal Server Error The server has encountered a situation it does not know how to handle.
 router.get('/users/:id', getUserById);
 
 // Update a User by ID
 router.put('/users/:id', updateUser);
 
 // Delete a User by ID
+// !500 Internal Server Error The server has encountered a situation it does not know how to handle.
 router.delete('/users/:id', deleteUser);
 
 // Add product
 // router.post('/products',addProduct);
 // router.get('/allproducts',getAllProducts)
+// !404 Not Found Requested resource could not be found. 
 router.post('/users/verify', verifyUserCredentials);
+
+// !404 Not Found Requested resource could not be found. 
 router.post('/users/passwordChanged', forgotPassword);
 
+// !404 Not Found Requested resource could not be found. 
 router.get('/users/:userId/products',getProductsForUser)
+
+
 router.post('/users/:userId/products',addProductForUser)
 router.put('/users/:userId/products/:productId',updateProductForUser)
+
+// !500 Internal Server Error The server has encountered a situation it does not know how to handle.
 router.delete('/users/:userId/products/:productId',deleteProductForUser)
 
 
-
-
+// !404 Not Found Requested resource could not be found. 
 router.route("/login").post(login);
+
+// !404 Not Found Requested resource could not be found. 
 router.route("/register").post(register);
 
 module.exports=router;
