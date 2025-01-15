ace583da56ece7ccc6c1dbaf10a093ce36bc576f
Kajal Sharma <kajalsharma86900@gmail.com>
2025-01-15 12:55:50 +0530

    create delete and update api for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,6 +17,37 @@ exports.addProduct = async (req, res) => {
   }
 };
 
+exports.updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  const { name, description, price } = req.body;
+
+  // Validate the input fields
+  if (!name || !description || !price) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  try {
+    // Attempt to find and update the product by its ID
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { productName: name, productDescription: description, productPrice: price },
+      { new: true } // This option returns the updated document
+    );
+
+    // If the product is not found
+    if (!updatedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Respond with the updated product data
+    res.status(200).json({ message: 'Product updated successfully', updatedProduct });
+  } catch (error) {
+    console.error('Error updating product:', error);
+    res.status(500).json({ error: 'Error updating product' });
+  }
+};
+
+
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
@@ -26,6 +57,26 @@ exports.getAllProducts = async (req, res) => {
     res.status(500).json({ error: 'Error getting products' });
   }
 };
+
+exports.deleteProduct = async (req, res) => {
+  const productId = req.params.productId;
+  try {
+    // Attempt to find and delete the product by its ID
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+    
+    // If the product is not found
+    if (!deletedProduct) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Respond with a success message and the deleted product data
+    res.status(200).json({ message: 'Product deleted successfully', deletedProduct });
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    res.status(500).json({ error: 'Error deleting product' });
+  }
+};
+
 
 exports.getProductById = async (req, res) => {
   const productId = req.params.productId;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router();
 
-const { addProduct, getAllProducts } = require('../controllers/productController');
+const { addProduct, getAllProducts, deleteProduct, updateProduct } = require('../controllers/productController');
 
 // Register a driver
 // ? Working
 router.post('/addNewProduct', addProduct);
 router.get('/getAllProducts', getAllProducts);
-
-
-
+router.delete('/products/:productId', deleteProduct);
+router.put('/products/:productId', updateProduct);
 
 module.exports=router;
