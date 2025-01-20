3c905da3f865e5664d0a0651a7dd9f48fb0302c6
Arbaz Khan <Iamakonarbaz@gmail.com>
2025-01-20 16:13:06 +0530

    email fix

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -2,7 +2,6 @@ const Transaction = require("../models/transaction");
 const Customer = require("../models/customer");
 const Driver = require("../models/driver");
 const User = require("../models/User");
-const nodemailer = require("nodemailer");
 
 // Create
 exports.createTransaction = async (req, res) => {
@@ -55,18 +54,7 @@ exports.createTransaction = async (req, res) => {
       bottlesCount,
     });
     await transaction.save();
-    // Compose email message
-    emailMessage += `Payments: ${paymentTaken}₹\n`;
-    emailMessage += `Due Amount: ${dueAmount}₹\n`;
-    emailMessage += `Transaction Date: ${dateTime}\n`;
-    emailMessage += `Delivered by: ${driverId}\n`; // Fixed the curly brace typo
-    await (async () => {
-      // Simulate an asynchronous operation
-      await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      await sendEmail(customer.email, "Transaction Details", emailMessage);
-      // Send email to customer
-    })();
     // Return the created transaction
     res.status(201).json({
       success: true,
@@ -90,38 +78,6 @@ exports.transactionHistory = async (req, res) => {
     res.status(500).json({ error: "Error fetching transactions" });
   }
 };
-
-// async function sendEmail(to, subject, text) {
-//   try {
-//     // Create Nodemailer transporter
-//     let transporter = nodemailer.createTransport({
-//       // Configure SMTP transporter
-//       host: "smtp.gmail.com", // SMTP server host (e.g., smtp.gmail.com)
-//       port: 587, // SMTP server port (587 for TLS, 465 for SSL)
-//       secure: false, // true for 465, false for other ports
-//       auth: {
-//         user: "sampming1@gmail.com", // SMTP username
-//         pass: "wbta gpcc cgsh dzvl", // SMTP password
-//       }, 
-//     });
-
-//     // Define email options
-//     let mailOptions = {
-//       from: "sampming1@gmail.com", // Sender email address
-//       to: to, // Recipient email address
-//       subject: subject, // Email subject
-//       text: text, // Email body
-//     };
-//     // console.log("send ", mailOptions);
-//     // return;
-//     // Send email
-//     let info = await transporter.sendMail(mailOptions);
-//     console.log("Email sent:", info.messageId);
-//   } catch (error) {
-//     console.error("Error sending email:", error);
-//     throw error; // Throw the error to handle it in the calling function
-//   }
-// }
 
 exports.customerTransactions = async (req, res) => {
   try {
